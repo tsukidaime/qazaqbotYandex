92252cb2bc06a50571b953f907e72c138cbc6119
Aibek Shulembekov <aibek1805@gmail.com>
2019-12-10 18:56:08 +0600

    webhook added

--- init.py
+++ init.py
@@ -41,9 +41,9 @@ PORT = int(os.environ.get('PORT', '8443'))
 dp = updater.dispatcher
 dp.add_handler(InlineQueryHandler(inline_aydary))
 dp.add_error_handler(error_callback)
-# updater.start_webhook(listen="0.0.0.0",
-#                       port=PORT,
-#                       url_path=TOKEN)
-# updater.bot.set_webhook("https://qazaqbot.herokuapp.com/" + TOKEN)
-updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://qazaqbot.herokuapp.com/" + TOKEN)
+# updater.start_polling()
 updater.idle()
